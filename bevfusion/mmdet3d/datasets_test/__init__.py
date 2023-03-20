b18528c31fe5a02df802852e4fef4910139ae369
Desmond Lew <desmondlew@Desmond-Lews-MacBook-Pro.local>
2023-03-21 01:42:19 +0800

    t

---
+++ bevfusion/mmdet3d/datasets_test/__init__.py
@@ -0,0 +1,8 @@
+from mmdet.datasets.builder import build_dataloader
+
+from .builder import *
+from .custom_3d import *
+from .nuscenes_dataset import *
+from .nuscenes_dataset_test import *
+from .pipelines import *
+from .utils import *
